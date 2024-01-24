8d29f4ad2aa7f33f89af9892fbbe330e1c0d3292
Ryan Wong <ryanjameswong@outlook.com>
2024-01-24 15:34:53 -0500

    Add picture to body and text sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("working");
+import Gif from './Restaurant.gif';
+import Menu from './Menu.png';
+
+
+const content = document.getElementById('content');
+const restaurantImageSection = document.createElement('section');
+const restaurantTextSection =  document.createElement('section');
+const restaurantMenuSection = document.createElement('section');
+
+
+const backgroundImage = new Image();
+backgroundImage.src = Gif;
+
+restaurantImageSection.appendChild(backgroundImage);
+
+restaurantTextSection.innerHTML = `
+  <h2>Taste of Ba Sing Se</h2>
+  <p>Flavours from all four nations.</p>
+`;
+
+const menuImage = new Image();
+menuImage.src = Menu;
+
+content.appendChild(restaurantImageSection);
+content.appendChild(restaurantTextSection);
+content.appendChild(restaurantMenuSection);
